24ef1c378302c17464d82b930ea60e78b2d69f11
Cole Wangler <cole.wangler@ndsu.edu>
2023-11-30 13:42:03 -0600

    Database

---
+++ Migrations/20231130193731_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment5.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MusicInventory",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    musicTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    performer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    typeOfDownload = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    yearReleased = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MusicInventory", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MusicInventory");
+        }
+    }
+}
