afb0cc39588328fce19989d61917118ea065afdd
Cole Wangler <cole.wangler@ndsu.edu>
2023-12-01 10:02:23 -0600

    Updated Admin

--- Models/MusicInventory.cs
+++ Models/MusicInventory.cs
@@ -1,13 +1,21 @@
-﻿namespace Assignment5.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment5.Models
 {
     public class MusicInventory
     {
         public int id { get; set; }
+        [Display(Name = "Music Title:")]
         public string musicTitle { get; set; }
+        [Display(Name = "Genre:")]
         public string genre { get; set; }
+        [Display(Name = "Performer:")]
         public string performer { get; set; }
+        [Display(Name = "Price")]
         public decimal price { get; set; }
+        [Display(Name = "Download Type:")]
         public string typeOfDownload { get; set; }
+        [Display(Name = "Year Released:")]
         public int yearReleased { get; set; }
     }
 }
